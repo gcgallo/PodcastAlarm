5c6a78781df87c0c18b617b3baf001af131fcae4
gcgallo <ggallo102@gmail.com>
2017-04-03 23:51:26 -0700

    slightly more functional

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PodcastAlarm
+# PodcastAlarm  
 (work in progress)
 a shell for playing the lastest episode of a podcast at a specific time
 
@@ -6,4 +6,32 @@ a shell for playing the lastest episode of a podcast at a specific time
 a reliable internet connection  
 cron  
 youtube-dl  
-mplayer  
+mplayer 
+
+# instructions  
+
+Choose a youtube channel, user, or playlist you would like 
+to use as your alarm. Ideally, this channel would only upload 
+one episode per day, or otherwise provides a playlist for the 
+particular show you wish to use.   
+ 
+Clone this repo  
+
+cd into the repo
+
+Run the following command:  
+'''./alarm.sh -a <alarm_num> -u <alarm_url> -s <hr:min>'''
+
+where:  
+'''
+alarm_num = abitrary identfier to distinguish multiple alarms  
+alarm_url = url of desired youtube channel/playlist  
+hr:min = time of day you wish to set alarm to trigger  
+'''
+
+# features to be added  
+key command for killing alarm, instead having to figure out PID  
+press any key snooze feature, variable or predefined snooze time,
+or maybe random snooze time???   
+generalize to work with any podcast, not just ones available on youtube.  
+integrate into my RasPi MP3 project?  
--- alarm.sh
+++ alarm.sh
@@ -6,36 +6,60 @@ usage(){
   exit 1 
 }
 
-add_alarm(){
-#choose alarm number and channel
-}
+# add_alarm(){
+# choose alarm number and channel
+# echo 1
+# }
 
 set_alarm(){
 #crontab choose time day
+  echo $alarm_num
+  echo $alarm_url
+  echo $1
+  $hr="echo $1 | awk -F: '{ print $1 }' | xargs echo"
+  $min="echo $1 | awk -F: '{ print $2 }' | xargs echo" 
+  echo $min
+  echo $hr
+  # (crontab -l 2>/dev/null; echo "$min $hr * * * /home/gg/alarm.sh -a $alarm_num -u $alarm_url -t") | crontab -
+}
+
+list_alarms(){
+#list all currently set alarms with alarm number?
+   crontab -l
 }
 
 trigger_alarm(){
-  youtube-dl --playlist-end 1 -x https://www.youtube.com/user/technewstoday
+  youtube-dl --playlist-end 1 -o "/home/gg/alarm/%(title)s.%(ext)s" -x $alarm_url
   mplayer *.opus &
-  echo $! > ./technews.pid
+  echo $! > ./alarm_$alarm_num.pid
 }
 
 kill_alarm(){
-  kill $(cat alarm.pid)
-  rm /home/gg/alarm/alarm.pid
+  kill $(cat alarm_$alarm_num.pid)
+  rm /home/gg/alarm/alarm_$alarm_num.pid
   rm /root/*.opus
 }
 
 snooze(){
-#crontab reset add x minutes default 10? or random?
+
+  read -n 1 -s -p "Press any key to continue"
+
 }
-while getopts "a:s:t:kz:" opt; do
+
+#delete_alarm(){
+  #delete alarm specified by alarm number
+#}
+
+while getopts "a:u:s:lt:kzd:" opt; do
   case $opt in
-     a ) add_alarm;;
-     s ) set_alarm;;
-     t ) trigger_alarm;;
+     a ) alarm_num=$OPTARG;;
+     u ) alarm_url=$OPTARG;; 
+     s ) set_alarm $OPTARG;;
+     l ) list_alarms;;
+     t ) trigger_alarm $OPTARG;;
      k ) kill_alarm;;
      z ) snooze;;
+     d ) delete_alarm $OPTARG;;
      * ) usage;;
   esac
 done
